eef3ddf2f6154ef5be8efef31b435886e3762af1
Letícia <leticiabsouza09@gmail.com>
2026-08-04 20:30:12 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -32,7 +32,5 @@ class Dog(Animal):
 
 
 def feed_animals(animals):
-    total_food = 0
-    for animal in animals:
-        total_food += animal.feed()
-    return total_food
+    return sum(animal.feed() for animal in animals)
+
